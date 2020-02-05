a107868819adc72ca2b737345c1af80e1b29498a
Rafael Matoso <rafael.gustavo.gomes@concrete.com.br>
2020-02-05 11:34:17 -0300

    Gravando dados na Store, Reducer, Actions Creators, Dispatch

--- src/pages/Add/index.js
+++ src/pages/Add/index.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addCar } from '../../store/cars';
 
 export default function Add() {
-	return (
-		<form className="container mt-5">
-			<div className="form-group">
-				<label>Nome</label>
-				<input type="text" name="name" className="form-control" placeholder="Nome..." />
-			</div>
-			<div className="form-group">
-				<label>URL:</label>
-				<input type="text" name="url" className="form-control" placeholder="URL:https://cars" />
-			</div>
-			<button type="submit" className="btn btn-primary">
-				Adicionar
-			</button>
-		</form>
-	);
+  const [form, setForm] = useState({ name: '', url: '' });
+  const dispatch = useDispatch();
+
+  function formChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    dispatch(addCar(form));
+
+    setForm({ name: '', url: '' });
+  }
+
+  return (
+    <form className="container mt-5" onSubmit={onSubmit}>
+      <div className="form-group">
+        <label>Nome</label>
+        <input
+          onChange={formChange}
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Nome..."
+          value={form.name}
+        />
+      </div>
+      <div className="form-group">
+        <label>URL:</label>
+        <input
+          onChange={formChange}
+          type="text"
+          name="url"
+          className="form-control"
+          placeholder="URL:https://cars"
+          value={form.url}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Adicionar
+      </button>
+    </form>
+  );
 }

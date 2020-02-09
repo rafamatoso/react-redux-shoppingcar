b02f7221a8f2fe4a9c5275a4f19c2653e8896322
Rafael Matoso <rafael.gustavo.gomes@concrete.com.br>
2020-02-09 20:39:38 -0300

    Integracao 1: Fetch Actions, Redux-Thunk, NodeJS

--- src/store/cars/index.js
+++ src/store/cars/index.js
@@ -1,31 +1,9 @@
-const INITIAL_STATE = [
-  {
-    name: 'Ferrari',
-    url:
-      'https://images.pexels.com/photos/337909/pexels-photo-337909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  },
-  {
-    name: 'Mercedes',
-    url:
-      'https://images.pexels.com/photos/120049/pexels-photo-120049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  },
-  {
-    name: 'Audi',
-    url:
-      'https://images.pexels.com/photos/1149831/pexels-photo-1149831.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  }
-];
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
-export default function reducer(state = INITIAL_STATE, action) {
-  if (action.type === 'ADD_CAR') {
-    return [...state, action.car];
-  }
-  return state;
-}
+const INITIAL_STATE = [];
 
-export const addCar = car => {
-  return {
-    type: 'ADD_CAR',
-    car
-  };
-};
+export const addCar = createAction('ADD_CAR');
+
+export default createReducer(INITIAL_STATE, {
+  [addCar.type]: (state, action) => [...state, action.payload]
+});

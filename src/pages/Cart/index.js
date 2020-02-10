b67b58f1084ce5a20ce79e5d51ed2b9d6e94aa19
Rafael Matoso <rafael.gustavo.gomes@concrete.com.br>
2020-02-10 12:05:08 -0300

    Implementando Carrinho de Compras Parte 1

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 export default function Cart() {
-	return <p className="mt-5 text-warning text-center">Sem produtos no Carrinho...</p>;
+  const cart = useSelector(state => state.cart);
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        {cart.length === 0 ? (
+          <p className="col-sm-12 mt-5 text-warning text-center">
+            Sem produtos no carrinho...
+          </p>
+        ) : (
+          <div className="col-sm-3 mt-3">
+            <div className="card text-white bg-primary mb-3">
+              <div className="card-header">Header</div>
+              <div className="card-body">
+                <h5 className="card-title">Primary card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Car from '../../components/Car';
 
 import { getAllCars } from '../../store/fetchActions';
+import { addItem } from '../../store/ducks/cart';
 
 export default function List() {
   const cars = useSelector(state => state.cars);
@@ -13,11 +14,15 @@ export default function List() {
     dispatch(getAllCars());
   }, [dispatch]);
 
+  function addItemCart(car) {
+    dispatch(addItem(car));
+  }
+
   return (
     <div className="container-fluid">
       <div className="row">
         {cars.map((car, index) => (
-          <Car key={index} car={car} />
+          <Car key={index} car={car} addItemCart={addItemCart} />
         ))}
       </div>
     </div>
